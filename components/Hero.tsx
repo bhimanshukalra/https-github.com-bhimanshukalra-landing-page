006ebeecf3deae195cbb43e3cf51c64abe1bbbf9
bhimanshukalra <bhimanshukalra@gmail.com>
2024-08-18 17:37:08 +0530

    Chore: decrease dealy in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,8 +14,8 @@ const Hero = () => {
         <TextGenerateEffect
           className="text-center text-[40px] md:text-2xl lg:text-6xl"
           words="The Scribe Co. is your content development engine. With us, you spend less time, money, and resources while developing more content."
-          duration={0.4}
-          staggeredDelay={0.1}
+          duration={0.01}
+          staggeredDelay={0.08}
         />
       </div>
     );
